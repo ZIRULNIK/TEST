a906a6a7c2dddab41c9aed6b12837e31c9ed9f14
ZIRULNIK <90558443+ZIRULNIK@users.noreply.github.com>
2021-09-29 14:49:31 +0300

    Add files via upload

---
+++ тест 5 задание.cpp
@@ -0,0 +1,52 @@
+﻿// тест 5 задание.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "Rus");
+	srand(time(NULL));
+	int a = rand() % 10;
+	int b = rand() % 10;
+	int c = rand() % 10;
+	int d = rand() % 10;
+
+	cout << "пароль сгененирован: ****" << endl;
+	cout << "пароль угадан:";
+	for (int i = 0;i <= a;i++)
+	{
+		if (i == a)
+		{
+			cout << i;
+		}
+	}
+	for (int i = 0;i <= b;i++)
+	{
+		if (i == b)
+		{
+			cout << "" << i;
+		}
+	}
+	for (int i = 0;i <= c;i++)
+	{
+		if (i == c)
+		{
+			cout << "" << i;
+		}
+	}
+				for (int i = 0;i <= d;i++)
+				{
+					if (i == d)
+					{
+						cout << "" << i;
+					}
+
+				}
+
+
+
+			}
+
+		
